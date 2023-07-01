27dbeb231736f425f0722c802f571ea219be08f6
Tap <116336366+varTapexe@users.noreply.github.com>
2023-06-30 21:21:51 -0700

    Update gender_change.py

--- gender_change.py
+++ gender_change.py
@@ -3,15 +3,13 @@ import struct
 
 from wizwalker import ClientHandler, utils
 
-utils.override_wiz_install_location(r'E:\Kingsisle Entertainment\Wizard101')
+path = input("What is your Wizard101 path? (Example: C:\Kingsisle Entertainment\Wizard101)")
+utils.override_wiz_install_location(rf'{path}')
 
-# Male: 1
-# Female: 0
-# Other???: 3
-target_gender = 3
+target_gender = False
 
 """
-This script will change every single player, pet, and mount into the selected gender.
+This script will change every wizard in your game into the chosen gender.
 """
 
 MODELINFOPATTERN = rb"\x44\x8B\x88\xB4\x00\x00\x00\x44" \
@@ -21,25 +19,58 @@ MODELINFOPATTERN = rb"\x44\x8B\x88\xB4\x00\x00\x00\x44" \
                    rb"\x45\x39\x48\x48\x74\x19"
 
 
-async def main():
-    async with ClientHandler() as ch:
-        clients = ch.get_new_clients()
-
-        # address should be the same for all clients
-        model_info_race = await clients[0].hook_handler.pattern_scan(
-            MODELINFOPATTERN,
-            module="WizardGraphicalClient.exe",
-        )
-        model_info_gender = model_info_race + 7
-
-        packed_gender = struct.pack("<I", target_gender)
-        packed_gender_bytes = b"\x41\xBA" + packed_gender + b"\x90"
-
-        for client in clients:
-            print(f"{packed_gender_bytes=}")
-            await client.hook_handler.write_bytes(model_info_gender, packed_gender_bytes)
+async def main(gender):
+    try:
+        target_gender = gender
+        if target_gender:
+            async with ClientHandler() as ch:
+                clients = ch.get_new_clients()
+        
+                # address should be the same for all clients
+                model_info_race = await clients[0].hook_handler.pattern_scan(
+                    MODELINFOPATTERN,
+                    module="WizardGraphicalClient.exe",
+                )
+                model_info_gender = model_info_race + 7
+        
+                packed_gender = struct.pack("<I", target_gender)
+                packed_gender_bytes = b"\x41\xBA" + packed_gender + b"\x90"
+        
+                for client in clients:
+                    print(f"{packed_gender_bytes=}")
+                    await client.hook_handler.write_bytes(model_info_gender, packed_gender_bytes)
+        else:
+            target_gender = input("Please choose a gender (Type M or F)")
+            if target_gender.lower() == "f":
+                target_gender = 0
+            elif target_gender.lower() == "m":
+                target_gender = 1
+            else:
+                print("Not a valid gender input.")
+                exit()
+            async with ClientHandler() as ch:
+                clients = ch.get_new_clients()
+        
+                # address should be the same for all clients
+                model_info_race = await clients[0].hook_handler.pattern_scan(
+                    MODELINFOPATTERN,
+                    module="WizardGraphicalClient.exe",
+                )
+                model_info_gender = model_info_race + 7
+        
+                packed_gender = struct.pack("<I", target_gender)
+                packed_gender_bytes = b"\x41\xBA" + packed_gender + b"\x90"
+        
+                for client in clients:
+                    print(f"{packed_gender_bytes=}")
+                    await client.hook_handler.write_bytes(model_info_gender, packed_gender_bytes)
+    except Exception as e:
+        if "root.wad not found" in e:
+            print(f"An error occured: {e}") 
+        else:
+            print(f"Invalid Wizard101 path.")   
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(main(target_gender))
 
